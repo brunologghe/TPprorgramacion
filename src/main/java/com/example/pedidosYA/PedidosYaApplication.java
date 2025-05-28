d3f429d9e8e9f7b9fd46de9e467858cceeaadfb5
brunologghe <160283035+brunologghe@users.noreply.github.com>
2025-05-27 22:30:18 -0300

    commit

---
+++ src/main/java/com/example/pedidosYA/PedidosYaApplication.java
@@ -0,0 +1,13 @@
+package com.example.pedidosYA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidosYaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedidosYaApplication.class, args);
+	}
+
+}
