608984ef05827f3449373e5c33610ebbd87b355e
felialvarez <felialvarezzz03@gmail.com>
2025-06-04 15:56:42 -0300

    commit a bruno

--- src/main/java/com/example/pedidosYA/Model/Direccion.java
+++ src/main/java/com/example/pedidosYA/Model/Direccion.java
@@ -17,4 +17,54 @@ public class Direccion {
     @JoinColumn(name = "cliente_id")
     private Cliente cliente;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public String getCodigoPostal() {
+        return codigoPostal;
+    }
+
+    public void setCodigoPostal(String codigoPostal) {
+        this.codigoPostal = codigoPostal;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Direccion() {
+    }
 }
--- src/main/java/com/example/pedidosYA/Service/DireccionService.java
+++ src/main/java/com/example/pedidosYA/Service/DireccionService.java
@@ -1,12 +1,39 @@
 package com.example.pedidosYA.Service;
 
+import com.example.pedidosYA.DTO.DireccionDTO.DireccionCrearDTO;
+import com.example.pedidosYA.DTO.DireccionDTO.DireccionDTO;
+import com.example.pedidosYA.Model.Cliente;
+import com.example.pedidosYA.Model.Direccion;
+import com.example.pedidosYA.Repository.ClienteRepository;
 import com.example.pedidosYA.Repository.DireccionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
 
 public class DireccionService {
 
     @Autowired
     private DireccionRepository direccionRepository;
+    @Autowired
+    private ClienteRepository clienteRepository;
 
+    public DireccionDTO crearDireccion(DireccionCrearDTO direccion){
+        Optional<Cliente>clienteOptional = clienteRepository.findById(direccion.getId());
 
+        if (clienteOptional.isEmpty()) {
+            throw new RuntimeException("Cliente no encontrado con ID: " + direccion.getId());
+        }
+
+        Cliente cliente = clienteOptional.get();
+        Direccion direccionNueva = new Direccion();
+        direccionNueva.setDireccion(direccion.getDireccion());
+        direccionNueva.setCiudad(direccion.getCiudad());
+        direccionNueva.setPais(direccion.getPais());
+        direccionNueva.setCliente(cliente);
+        direccionNueva.setCodigoPostal(direccion.getCodigoPostal());
+
+        Direccion direccionRetorno = direccionRepository.save(direccionNueva);
+
+        return new DireccionDTO(direccionRetorno.getId(), direccionRetorno.getDireccion(), direccionRetorno.getCiudad(), direccionRetorno.getPais(), direccionRetorno.getCodigoPostal());
+    }
 }
