e989a6f4187c328d5966ed866f95db09ecf7f6d0
Lucasmonti <lucasmonti014@gmail.com>
2025-06-05 15:01:42 -0300

    producto service

--- src/main/java/com/example/pedidosYA/Repository/ProductoRepository.java
+++ src/main/java/com/example/pedidosYA/Repository/ProductoRepository.java
@@ -4,5 +4,8 @@ import com.example.pedidosYA.Model.Cliente;
 import com.example.pedidosYA.Model.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductoRepository extends JpaRepository<Producto, Long> {
+    public List<Producto> findAllByRestauranteId(Long id);
 }
