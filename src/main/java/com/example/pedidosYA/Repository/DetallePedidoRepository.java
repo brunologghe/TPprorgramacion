b1c328b1b287893333b8e724a111d157a3c4075f
felialvarez <felialvarezzz03@gmail.com>
2025-06-07 19:00:01 -0300

    Delete DetallePedidoRepository.java

--- src/main/java/com/example/pedidosYA/Repository/DetallePedidoRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.pedidosYA.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DetallePedidoRepository extends JpaRepository<DetallePedido, Long> {
-}
