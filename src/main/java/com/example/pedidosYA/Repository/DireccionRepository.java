ee2533633f1eacc465d590128d978ae96aea7a16
brunologghe <160283035+brunologghe@users.noreply.github.com>
2025-06-08 22:00:41 -0300

    a

--- src/main/java/com/example/pedidosYA/Repository/DireccionRepository.java
+++ src/main/java/com/example/pedidosYA/Repository/DireccionRepository.java
@@ -4,6 +4,8 @@ import com.example.pedidosYA.Model.Cliente;
 import com.example.pedidosYA.Model.Direccion;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface DireccionRepository extends JpaRepository<Direccion, Long> {
     Direccion findByClienteIdAndCodigoPostalAndDireccion(Long clienteId, String codigoPostal, String direccion);
     List<Direccion> findByClienteId(Long id);
