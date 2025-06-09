bbcfa2a71278d9bce27873ba3522cbcd95a69c53
brunologghe <160283035+brunologghe@users.noreply.github.com>
2025-06-08 22:59:45 -0300

    bm de cliente y restaurante en admin

--- src/main/java/com/example/pedidosYA/Controller/AdminController.java
+++ src/main/java/com/example/pedidosYA/Controller/AdminController.java
@@ -2,9 +2,14 @@ package com.example.pedidosYA.Controller;
 
 import com.example.pedidosYA.DTO.AdminDTO.AdminDetailDTO;
 import com.example.pedidosYA.DTO.AdminDTO.AdminRequestDTO;
+import com.example.pedidosYA.DTO.ClienteDTO.ModificarDTO;
 import com.example.pedidosYA.DTO.ClienteDTO.ResponseDTO;
+import com.example.pedidosYA.DTO.RestauranteDTO.RestauranteModificarDTO;
+import com.example.pedidosYA.DTO.RestauranteDTO.RestauranteResponseDTO;
+import com.example.pedidosYA.DTO.RestauranteDTO.RestauranteResumenDTO;
 import com.example.pedidosYA.Service.AdminService;
 import com.example.pedidosYA.Service.ClienteService;
+import com.example.pedidosYA.Service.RestauranteService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -12,6 +17,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Set;
 
 
 @RestController
@@ -22,6 +28,8 @@ public class AdminController {
     private AdminService adminService;
     @Autowired
     private ClienteService clienteService;
+    @Autowired
+    private RestauranteService restauranteService;
 
 
     @PostMapping
@@ -31,13 +39,39 @@ public class AdminController {
     }
 
     @GetMapping("/cliente")
-    public ResponseEntity<List<ResponseDTO>> listAll(){
+    public ResponseEntity<List<ResponseDTO>> listAllClientes(){
         return ResponseEntity.ok(clienteService.listAll());
     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<ResponseDTO> delete(@PathVariable Long id){
+    @DeleteMapping("/cliente/{id}")
+    public ResponseEntity<ResponseDTO> deleteCliente(@PathVariable Long id){
         return ResponseEntity.ok(clienteService.eliminar(id));
+    }
+
+    @PutMapping ("/cliente/{id}")
+    public ResponseEntity<ResponseDTO> modificarCliente (@PathVariable Long id, @Valid @RequestBody ModificarDTO modificarDTO){
+
+        ResponseDTO cliente = clienteService.modificar(id, modificarDTO);
+
+        return ResponseEntity.ok(cliente);
+    }
+
+    @GetMapping("/restaurante")
+    public ResponseEntity<Set<RestauranteResumenDTO>> listAllRestaurantes(){
+        return ResponseEntity.ok(restauranteService.findAllRestaurantes());
+    }
+
+    @DeleteMapping("/restaurante/{id}")
+    public ResponseEntity<RestauranteResponseDTO> deleteRestaurante(@PathVariable Long id){
+        return ResponseEntity.ok(restauranteService.eliminarRestaurante(id));
+    }
+
+    @PutMapping ("/restaurante/{id}")
+    public ResponseEntity<RestauranteResponseDTO> modificarRestaurante (@PathVariable Long id, @Valid @RequestBody RestauranteModificarDTO restauranteModificarDTO){
+
+        RestauranteResponseDTO bodyRestaurante = restauranteService.modificarRestaurante(id, restauranteModificarDTO);
+
+        return ResponseEntity.ok(bodyRestaurante);
     }
 
 
--- src/main/java/com/example/pedidosYA/Service/ClienteService.java
+++ src/main/java/com/example/pedidosYA/Service/ClienteService.java
@@ -1,8 +1,13 @@
 package com.example.pedidosYA.Service;
 
 import com.example.pedidosYA.DTO.ClienteDTO.ClienteCrearDTO;
+import com.example.pedidosYA.DTO.ClienteDTO.ModificarDTO;
 import com.example.pedidosYA.DTO.ClienteDTO.ResponseDTO;
+import com.example.pedidosYA.DTO.RestauranteDTO.RestauranteModificarDTO;
+import com.example.pedidosYA.DTO.RestauranteDTO.RestauranteResponseDTO;
+import com.example.pedidosYA.Exceptions.BusinessException;
 import com.example.pedidosYA.Model.Cliente;
+import com.example.pedidosYA.Model.Restaurante;
 import com.example.pedidosYA.Model.Usuario;
 import com.example.pedidosYA.Repository.ClienteRepository;
 import com.example.pedidosYA.Validations.ClienteValidations;
@@ -49,4 +54,20 @@ public class ClienteService {
         clienteRepository.deleteById(id);
         return clienteDTO;
     }
+
+    public ResponseDTO modificar (Long id, ModificarDTO clienteNuevo){
+        clienteValidations.validarContraseniaActual(id, clienteNuevo.getContraseniaActual());
+
+        Cliente cliente = clienteRepository.findById(id).orElseThrow(() -> new BusinessException("Cliente no encontrado"));
+
+        cliente.setNombreYapellido(clienteNuevo.getNombreYapellido());
+        cliente.setUsuario(clienteNuevo.getUsuario());
+        cliente.setContrasenia(clienteNuevo.getContraseniaNueva());
+
+        clienteRepository.save(cliente);
+
+        return new ResponseDTO(cliente.getId(), cliente.getUsuario(), cliente.getNombreYapellido());
+
+    }
+
 }
