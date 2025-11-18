480932861aafd0c5eb99812bc97fe05b741672ab
Lucasmonti <lucasmonti014@gmail.com>
2025-11-18 18:38:24 -0300

    mejora put usuario, email agregado a usuario, mejora en endpoints, nueva funcionalidad(verRestauranteDetail como cliente)

---
+++ src/main/java/com/example/pedidosYA/DTO/RestauranteDTO/ActualizarPerfilRestauranteDTO.java
@@ -0,0 +1,41 @@
+package com.example.pedidosYA.DTO.RestauranteDTO;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class ActualizarPerfilRestauranteDTO {
+    
+    @Size(min = 3, max = 25, message = "El nombre del restaurante debe tener entre 3 y 25 caracteres")
+    private String nombreRestaurante;
+    
+    @Email(message = "El formato del email no es válido")
+    private String email;
+    
+    @NotBlank(message = "La contraseña actual es obligatoria para verificar la identidad")
+    private String contraseniaActual;
+
+    public String getNombreRestaurante() {
+        return nombreRestaurante;
+    }
+
+    public void setNombreRestaurante(String nombreRestaurante) {
+        this.nombreRestaurante = nombreRestaurante;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContraseniaActual() {
+        return contraseniaActual;
+    }
+
+    public void setContraseniaActual(String contraseniaActual) {
+        this.contraseniaActual = contraseniaActual;
+    }
+}
